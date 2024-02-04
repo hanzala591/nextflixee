8c99041dfeea83fd83e8d2a7cf50ead1f57d147e
hanzala591 <raihanzala591@gmail.com>
2024-02-04 21:05:37 +0500

    add question answer feature

--- src/Component/FrequentQAs.js
+++ src/Component/FrequentQAs.js
@@ -1,34 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import { faPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
 
-export default function FrequentQAs() {
+export default function FrequentQAs(props) {
+  const questionAnswer = props.questionAnswer;
+  let [icon, setIcon] = useState(faPlus);
+  const [display, setDisplay] = useState("hidden");
+  const opened = (event) => {
+    if (display === "hidden") {
+      setDisplay("");
+      setIcon(faXmark);
+    } else {
+      setDisplay("hidden");
+      setIcon(faPlus);
+    }
+  };
   return (
     <div>
-      <div className="w-[100%] mt-3 mb-2 bg-gray-900 hover:bg-gray-700 hover:cursor-pointer">
+      <div
+        className="w-[100%] mt-3 bg-gray-900 hover:bg-gray-700 hover:cursor-pointer"
+        onClick={opened}
+      >
         <div className="flex p-[20px_10px] justify-between items-center">
-          <p className="sm:text-2xl text-xl">What is Netflix?</p>
-          <FontAwesomeIcon icon={faPlus} size="2x" />
-        </div>
-        <div className="h-[3px] bg-black"></div>
-        <div className="p-[20px_10px] items-center">
-          <p>
-            Netflix is a streaming service that offers a wide variety of
-            award-winning TV shows, movies, anime, documentaries, and more on
-            thousands of internet-connected devices.{" "}
-          </p>
-          <p>
-            You can watch as much as you want, whenever you want without a
-            single commercial – all for one low monthly price. There's always
-            something new to discover and new TV shows and movies are added
-            every week!
-          </p>
+          <p className="sm:text-2xl text-xl">{questionAnswer.question}</p>
+          <FontAwesomeIcon icon={icon} size="2x" />
         </div>
       </div>
-      <div className="w-[100%] mt-3 mb-2 bg-gray-900 hover:bg-gray-700 hover:cursor-pointer">
-        <div className="flex p-[20px_10px] justify-between items-center">
-          <p className="sm:text-2xl text-xl">What is Netflix?</p>
-          <FontAwesomeIcon icon={faPlus} size="2x" />
+      <div className="w-[100%] bg-gray-900">
+        <div className={display}>
+          <div className="h-[3px] bg-black"></div>
+          <div className="p-[20px_10px] items-center">
+            <p>{questionAnswer.answer}</p>
+          </div>
         </div>
       </div>
     </div>
--- src/Component/Question.js
+++ src/Component/Question.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import FrequentQAs from "./FrequentQAs";
 
 export default function Question() {
+  const [questionAnswer, setQuestionAnswer] = useState([
+    {
+      question: "What is Netflix?",
+      answer: `Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!`,
+    },
+    {
+      question: "How much does Netflix cost?",
+      answer:
+        "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from Rs 250 to Rs 1,100 a month. No extra costs, no contracts.",
+    },
+    {
+      question: "Where can I Watch?",
+      answer:
+        "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
+    },
+    {
+      question: "How do I cancel?",
+      answer:
+        "Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+    },
+    {
+      question: "What can I Watch on Netflix?",
+      answer:
+        "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.",
+    },
+    ,
+    {
+      question: "Is Netflix good for Kids?",
+      answer:
+        "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
+    },
+  ]);
+  let FrequentQA = questionAnswer.map((currentValue, index) => {
+    return (
+      <FrequentQAs
+        questionAnswer={questionAnswer[index]}
+        index={index}
+        key={index}
+      ></FrequentQAs>
+    );
+  });
   return (
     <div>
       <div className="w-[100%] py-[100px] bg-black ">
@@ -9,7 +50,7 @@ export default function Question() {
           <h1 className="text-center sm:text-5xl text-3xl font-bold pb-4">
             Frequently Asked Questions
           </h1>
-          <FrequentQAs></FrequentQAs>
+          {FrequentQA}
         </div>
       </div>
     </div>
